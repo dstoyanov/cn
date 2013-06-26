284c1f4b6583b794c5428aa73644c36adeed7dd5
Dimo Stoyanov <stoyanov.dimo@googlemail.com>
2013-06-26 22:57:06 +0200

    chat

--- cn/cn/src/nl/vu/cs/cn/Chat.java
+++ cn/cn/src/nl/vu/cs/cn/Chat.java
@@ -4,25 +4,29 @@ import java.io.IOException;
 
 import nl.vu.cs.cn.IP.IpAddress;
 import nl.vu.cs.cn.TCP.Socket;
-
-//import android.R;
-import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Chat extends Activity {
-	
-	Socket s1, s2;
-	EditText editText1, editText2;
-	
+public class Chat extends Activity{
+
+	Server server;
+	Client client;
+
+	int port1 = 1234;
+	int port2 = 4321;
+
+	int addr1 = 1;
+	int addr2 = 2;
+
+	int bufsize = 100;
+
+	TextView tv1;
+	TextView tv2;
 
 
 	/** Called when the activity is first created. */
@@ -30,42 +34,140 @@ public class Chat extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		
-//		LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayout1);
-//		editText1 = (EditText) ll.findViewById(R.id.textInput1);
-//		editText1 = (EditText) findViewById(R.id.textInput1);
 
-//		editText1.setText("lalalla1");
 
-		
-//		editText1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//
-//		    public void onFocusChange(View v, boolean hasFocus) {
-//		    	InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-//		    	mgr.showSoftInput(editText1, InputMethodManager.SHOW_IMPLICIT);
-//		    }
-//		});
-		System.out.println("fafa");
-		
-		
-        editText1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            public void onFocusChange(View v, boolean hasFocus) {
-                editText1.post(new Runnable() {
-                    public void run() {
-                    	editText1.setText("Focus change");
-                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                       imm.showSoftInput(editText1, InputMethodManager.SHOW_IMPLICIT);
-                    }
-                });
-            }
-        });
+		tv1 = (TextView) findViewById(R.id.textView1);
+		tv2 = (TextView) findViewById(R.id.textView2);
 
-		
+		server = new Server(addr1, port1);
+		client = new Client(addr2, addr1, port1);
 
-		// Connect various GUI components to the networking stack.
+		new Thread(server).start();
+		new Thread(client).start();
+
 	}
-	
+
+	public void sendMessage1(View view){
+		EditText et = (EditText) findViewById(R.id.editText1);
+//		byte[] message = et.getText().toString().getBytes();
+
+		//		client.write_socket.write(message, 0, message.length);
+		et.setText("text");
+	}
+
+	public void sendMessage2(View view){
+		EditText et = (EditText) findViewById(R.id.editText2);
+//		byte[] message = et.getText().toString().getBytes();
+
+		//		server.write_socket.write(message, 0, message.length);
+		et.setText("text");
+	}
+
+	Handler handler = new Handler(){
+		public void handleMessage(Message msg){
+			TextView tv;
+			String text = (String) msg.obj;
+			int dst = msg.arg1;
 
 
+			if(dst == 1)
+				tv1.append(text);
+			else if(dst == 2)
+				tv2.append(text);
+
+		}
+	};
+
+	public class Server implements Runnable{
+
+		private TCP tcp;
+		private Socket read_socket;
+		private Socket write_socket;
+
+		public Server(int addr, int port1){
+			try{
+				tcp = new TCP(addr);
+				tv1.append("New TCP stack created (192.168.0." + addr + ").\n");
+
+				read_socket = tcp.socket(port1 + 1);
+				write_socket = tcp.socket(port1);
+
+				tv1.append("A socket listening on port " + port1 + " created.\n");
+
+
+			} catch(IOException e){
+
+			}
+		}
+
+		public void run() {
+			Message msg = new Message();
+
+			read_socket.accept();
+			write_socket.accept();
+			
+			msg.obj = "Connection Established\n";
+			msg.arg1 = 1;
+			handler.sendMessage(msg);
+			
+			while(true){}
+			//			
+			//			byte[] buf = new byte[bufsize];
+			//			
+			//			while(true){
+			//				read_socket.read(buf, 0, bufsize);
+			//				this.tv.append(buf.toString());
+			//			}
+		}
+	}
+
+	private class Client implements Runnable{
+
+		private TCP tcp;
+		private Socket read_socket;
+		private Socket write_socket;
+		private int dstAddress;
+
+		public Client(int srcAddress, int dstAddress, int port1){
+
+			try{
+
+				tcp = new TCP(srcAddress);
+				tv2.append("New TCP stack created (192.168.0." + dstAddress + ").\n");
+
+				read_socket = tcp.socket();
+				write_socket = tcp.socket();
+				tv2.append("A socket created.\n");
+
+			} catch(IOException e){
+
+			}
+			this.dstAddress = dstAddress;
+		}
+
+		public void run() {
+
+			Message msg = new Message();
+
+			read_socket.connect(IpAddress.getAddress("192.168.0." + dstAddress), port1);
+			write_socket.connect(IpAddress.getAddress("192.168.0." + dstAddress), port1 + 1);
+
+			msg.obj = "Connection Established\n";
+			msg.arg1 = 2;
+			handler.sendMessage(msg);
+
+			while(true){}
+			//			tv.append("Connection established.\n");
+			//			
+			//			byte[] buf = new byte[bufsize];
+			//			
+			//			
+			//			//TODO socket.isConnected?
+			//			while(true){
+			//				read_socket.read(buf, 0, bufsize);
+			//
+			//				this.tv.append(buf.toString());
+			//			}
+		}
+	}
 }
